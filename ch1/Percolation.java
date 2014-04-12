1bbd9375cf9859ebc8fe6f37d3fc5950ef32fcc8
= <=>
2014-04-12 16:40:45 +0800

    fix percolation isFull()

--- ch1/Percolation.java
+++ ch1/Percolation.java
@@ -1,18 +1,45 @@
+/*----------------------------------------------------------------
+ *  Author:        Leesper
+ *  Written:       4/12/2014
+ *
+ *  Compilation:   javac Percolation.java
+ *  Execution:     java Percolation
+ *  
+ *  My solution to the programming assignments in Week 1 for "Algorithms" by Sedgewick
+ *----------------------------------------------------------------*/
+
 public class Percolation {
 	public Percolation(int N) {
 		if (N < 1) {
 			throw new IllegalArgumentException("N must be >= 1");
 		}
 		size = N;
-		wquf = new WeightedQuickUnionUF(size * size);
-		op = new boolean[size * size];
-		for (int i = 0; i < size * size; i++) {
-			op[i] = false;
-		}
+		wquf = new WeightedQuickUnionUF(size * size + 2);
+		// op[vtop] is virtual top and op[vbot] is virtual bottom
+		op = new boolean[size * size + 2];
+		vtop = 0;
+		vbot = size * size + 1;
 	}
 	
+	/**
+	 *   Open the (i, j) site in N-by-N grid.
+	 */
 	public void open(int i, int j) {
-		if (!isOpen(i, j)) op[xyTo1D(i, j)] = true;
+		if ( isOpen(i, j) ) 
+			return;
+		
+		op[xyTo1D(i, j)] = true;
+		
+		if (isTopRow(i)) {
+			op[vtop] = true;
+			// make sites in top row union to virtual top
+			wquf.union(vtop, xyTo1D(i, j));
+		}
+		if (isBotRow(i)) {
+			op[vbot] = true;
+			// make sites in bottom row union to virtual bottom
+			wquf.union(vbot, xyTo1D(i, j));
+		}
 
 		// up
 		if (checkValid(i - 1, j) && isOpen(i - 1, j) && 
@@ -36,6 +63,9 @@ public class Percolation {
 		}
 	}
 	
+	/**
+	 *   return true if the (i, j) site in N-by-N grid is open.
+	 */
 	public boolean isOpen(int i, int j) { 
 		if (!checkValid(i, j)) {
 			throw new IndexOutOfBoundsException("index i out of range");
@@ -43,19 +73,53 @@ public class Percolation {
 		return op[xyTo1D(i, j)] == true;
 	}
 	
+	/**
+	 *   return true if the (i, j) site in N-by-N grid is full.
+	 *   A full site is an open site that can be connected to an 
+	 *   open site in the top row via a chain of neighboring 
+	 *   (left, right, up, down) open sites.	 
+	 */
 	public boolean isFull(int i, int j) { 
-		if (!checkValid(i, j)) {
-			throw new IndexOutOfBoundsException("index i out of range");
+		if (!isOpen(i, j)) {
+			return false;
 		}
-		return op[xyTo1D(i, j)] == false;
-		
+		// up
+		if ( checkValid(i - 1, j) && wquf.connected(vtop, xyTo1D(i - 1, j)) )
+		{
+			return true;
+		}
+		// down
+		if ( checkValid(i + 1, j) && wquf.connected(vtop, xyTo1D(i + 1, j)) )
+		{
+			return true;
+		}
+		// left
+		if ( checkValid(i, j - 1) && wquf.connected(vtop, xyTo1D(i, j - 1)) )
+		{
+			return true;
+		}
+		// right
+		if ( checkValid(i, j + 1) && wquf.connected(vtop, xyTo1D(i, j + 1)) )
+		{
+			return true;
+		}
+		return false;
 	}
 	
-	public boolean percolates() { return true; }
+	/**
+	 *   return true if this N-by-N system is percolated
+	 */
+	public boolean percolates() { 
+		return wquf.connected(vtop, vbot);
+	}
 	
 	// convert 2D index into 1D index (i, j) -> (i-1)*N+(j-1)
-	private int xyTo1D(int i, int j) { return (i - 1) * size + (j - 1); }
+	private int xyTo1D(int i, int j) { return (i - 1) * size + j; }
 	
+	/**
+	 *   return false if either i or j is out of range
+	 *   note that i/j must be [1, N]
+	 */
 	private boolean checkValid(int i, int j) {
 		if (i <= 0 || i > size) {
 			return false;
@@ -66,17 +130,29 @@ public class Percolation {
 		return true;
 	}
 	
+	private boolean isTopRow(final int i) {
+		return i == 1;
+	}
+	
+	private boolean isBotRow(final int i) {
+		return i == size;
+	}
+	
 	private WeightedQuickUnionUF wquf;
 	private final int size;
 	private boolean[] op; // mark whether it is open or blocked
+	private final int vtop;
+	private final int vbot;
 	
 	public static void main(String[] args) {
-		Percolation p = new Percolation(4);
-		StdOut.printf("(1, 1) status %b\n", p.isOpen(1, 1));
-		StdOut.printf("(1, 2) status %b\n", p.isOpen(1, 2));
-		p.open(1, 1);
-		p.open(1, 2);
-		StdOut.printf("(1, 1) status %b, (1, 2) status %b, connected %b\n",
-				p.isOpen(1, 1), p.isOpen(1, 2), p.wquf.connected(p.xyTo1D(1, 1), p.xyTo1D(1, 2)));
+		In in = new In(args[0]);      // input file
+        int N = in.readInt();         // N-by-N percolation system
+        Percolation perc = new Percolation(N);
+        StdOut.printf("is full: %b\n", perc.isFull(1, 1));
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            perc.open(i, j);
+        }
 	}
 }
